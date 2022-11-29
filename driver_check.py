d17c93f723f53c19fdd046302e2cb129290cb166
Ryan <ryanberrypi@protonmail.com>
2022-11-29 15:17:33 -0600

    Set up Requests library and verified request to relevant Nvidia page

--- driver_check.py
+++ driver_check.py
@@ -1,3 +1,16 @@
 # Created by Ryan McChesney
 # Created on 11/29/2022
 # Simple Nvidia driver checker
+
+import requests
+
+#To just get the listed version
+def getVersion():
+    r = requests.get('https://www.nvidia.com/download/driverResults.aspx/194380/en-us/')
+    print(r.text)
+
+def main():
+    getVersion()
+
+if __name__ == '__main__':
+    main()
